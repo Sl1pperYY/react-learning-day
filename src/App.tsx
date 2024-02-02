17f45339009e796b2da9ad75b3e6a29450f873bf
Vivien Kadar <vivien.kadar@lightware.com>
2024-02-02 11:17:16 +0100

    Add eventBus

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import GridComponent from './components/GridComponent/GridComponent';
+import { EventBus } from './lowlevel/EventBus';
 import './App.css';
 
 function App() {
+  const eventBus: EventBus = EventBus.getInstance();
+
   return (
     <div className="App">
       <GridComponent />
